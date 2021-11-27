01cfbafd7503e45b462eacbc6bea251c12822477
matias <matifandy@gmail.com>
2021-11-27 13:20:49 -0300

    first commit

---
+++ screens/DiscoverScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View ,SafeAreaView} from 'react-native'
+
+const DiscoverScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>Discover page</Text>
+        </SafeAreaView>
+    )
+}
+
+export default DiscoverScreen
+
+const styles = StyleSheet.create({})
